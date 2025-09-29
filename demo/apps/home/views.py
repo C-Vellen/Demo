b40e24024ca71f13439cade209735f2f4c2ce8be
Christophe Vellen <christophe.vellen@gmail.com>
2025-09-29 21:22:51 +0200

    prompt analyse de sentiments - distilbert

--- demo/apps/home/views.py
+++ demo/apps/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .context import usercontext
+from .forms import PromptForm
+from .output import model_output
 
 
 @login_required
@@ -10,6 +12,16 @@ def index(request):
         {
             "titre_onglet": "Démo",
             "titre_presentation": "Démonstration",
+            "response": "",
+            "request": False,
         }
     )
+    form = PromptForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        prompt = form.cleaned_data["prompt"]
+        response = model_output(prompt)
+        context.update({"prompt": prompt, "response": response, "request": True})
+
+    context.update({"form": form})
+
     return render(request, "home/index.html", context)
